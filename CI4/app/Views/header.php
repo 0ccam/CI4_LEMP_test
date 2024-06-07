a674e8b8c74e5d4c912b88d2e880fcffd24b4491
0ccam <dim.makeev@gmail.com>
2024-06-07 18:34:07 +0300

    header

--- CI4/app/Views/header.php
+++ CI4/app/Views/header.php
@@ -1,41 +1,12 @@
 <?php
 
-echo '<style>
-input[type=text], select {
-  width: 100%;
-  padding: 12px 20px;
-  margin: 8px 0;
-  display: inline-block;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  box-sizing: border-box;
-}
+/**=/
+/**/
+echo '<link rel="stylesheet" type="text/css" href="' . base_url() . 'css/style.css">';
+//echo '<link rel="stylesheet" type="text/css" href="' . base_url() . 'css/form.css">';
 
-input[type=submit] {
-  width: 100%;
-  background-color: #4CAF50;
-  color: white;
-  padding: 14px 20px;
-  margin: 8px 0;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-}
-
-input[type=submit]:hover {
-  background-color: #45a049;
-}
-
-div {
-  border-radius: 5px;
-  background-color: #f2f2f2;
-  padding: 20px;
-}
-</style>';
-
-//echo '<link rel="stylesheet" type="text/css" href="' . base_url() . 'css/table.css">';
-echo '<link rel="stylesheet" type="text/css" href="' . base_url() . 'css/form.css">';
-
+echo '<div class="topnav">';
 echo anchor('', 'Поиск');
 echo anchor('add', 'Добавить');
 echo anchor('open', 'Открыть');
+echo '</div>';
